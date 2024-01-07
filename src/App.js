d3e17cfd708c880705d170c0d50518842802ae42
wondernwadei <155618698+wondernwadei@users.noreply.github.com>
2024-01-08 00:02:40 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Form from "./components/Form";
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  return (
+    <div className="App">
+      <Header></Header>
+      <Form
+       todo={todo}
+       setTodo={setTodo}
+       todoList={todoList}
+       setTodoList={setTodoList}
+       ></Form>
+       <TodoList setTodoList={setTodoList} todoList={todoList}/>
+    </div>
+  );
+}
+
+export default App;
